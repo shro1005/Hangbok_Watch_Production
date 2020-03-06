52d75184bfc93ad2afe0a67bd4b5425935a451d9
shro1005 <shro1004@gmail.com>
2020-03-07 08:54:55 +0900

    2020.03.07 deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
-BUILD_PATH=$(ls /home/ec2-user/app/pro/build/*.jar)
-JAR_NAME=$(basename $BUILD_PATH)
-echo "> build 파일명: $JAR_NAME"
+REPOSITORY=/home/ec2-user/app/pro/
 
 echo "> build 파일 복사"
-DEPLOY_PATH=/home/ec2-user/app/pro/
-cp $BUILD_PATH $DEPLOY_PATH
-
-echo "> springboot-deploy.jar 교체"
-CP_JAR_PATH=$DEPLOY_PATH$JAR_NAME
-APPLICATION_JAR_NAME=springboot-deploy.jar
-APPLICATION_JAR=$DEPLOY_PATH$APPLICATION_JAR_NAME
-
-ln -Tfs $CP_JAR_PATH $APPLICATION_JAR
+cp $REPOSITORY/build/*.jar $REPOSITORY
 
 echo "> 현재 실행중인 애플리케이션 pid 확인"
-CURRENT_PID=$(pgrep -f $APPLICATION_JAR_NAME)
+CURRENT_PID=$(pgrep -fl backend | grep jar | awk '{pring $1}')
+
+echo "> 현재 구동중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z $CURRENT_PID ]
 then
@@ -26,8 +18,19 @@ else
   sleep 5
 fi
 
-echo "> $APPLICATION_JAR 배포"
+echo "> 새 애플리케이션 배포"
+
+JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
+
+echo "> JAR name : $JAR_NAME"
+
+echo "> $JAR_NAME 에 실행권한 추가"
+
+chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
+
 nohup java -jar \
         -Dspring.config.location=classpath:/application.yml \
         -Dspring.profiles.active=production \
-        $APPLICATION_JAR > $DEPLOY_PATHnohup.out 2>&1 &
+        $JAR_NAME > $DEPLOY_PATHnohup.out 2>&1 &
